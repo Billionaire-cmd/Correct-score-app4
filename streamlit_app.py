fa1857b25b5e13029174d9ed0be66d1ee60103d6
Billionaire-cmd <rabbigyimah7@gmail.com>
2024-12-05 23:11:32 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
+import yfinance as yf
+stock = st.text_input("Enter Stock Ticker:", "AAPL")
+data = yf.download(stock, start="2023-01-01")
+st.line_chart(data['Close'])
+
 ma_period = st.slider("Moving Average Period", 5, 100, 20)
 data['MA'] = data['Close'].rolling(ma_period).mean()
 st.line_chart(data[['Close', 'MA']])
